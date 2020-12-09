7b3880d4bb62d5e72c8765e6dbb2f002f4bd264b
ausaki <ljm51689@gmail.com>
2020-12-09 17:23:42 +0800

    lab6: update

--- net/output.c
+++ net/output.c
@@ -1,5 +1,7 @@
 #include "ns.h"
 
+#define MAXTRY 10
+
 extern union Nsipc nsipcbuf;
 
 void
@@ -11,14 +13,15 @@ output(envid_t ns_envid)
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
 	envid_t from_env;
-	int perm, r;
+	int perm, r, ntry;
 	while(1){
 		if((r = ipc_recv(&from_env, &nsipcbuf, &perm)) < 0 || r != NSREQ_OUTPUT){
 			continue;
 		}
-		if((r = sys_net_tx_packet(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len)) < 0){
-			;
+		ntry = 0;
+		while((r = sys_net_tx_packet(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len)) == -E_NETWORK_NO_BUF && ntry < MAXTRY){
+			sys_yield();
+			ntry++;
 		}
 	}
-	
 }
